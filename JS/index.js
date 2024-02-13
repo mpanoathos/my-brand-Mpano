7729c38dc699f57e206370d94b3379c2f9168832
mpanoathos <athosmpano427@gmail.com>
2024-02-14 01:19:13 +0200

    Made modifications on responsiveness and hamburger menus

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.querySelector("nav");
+    const hamburgerMenu = document.getElementById("hamburger-menu");
+
+    hamburgerMenu.addEventListener("click", function () {
+        // Toggle the visibility of the navigation bar
+        navbar.classList.toggle("show-navbar");
+
+        // Toggle the appearance of the hamburger menu icon
+        hamburgerMenu.classList.toggle("active");
+    });
+});
 
 //Downloading CV
 document.querySelector('.cv').addEventListener('click',()=>{
