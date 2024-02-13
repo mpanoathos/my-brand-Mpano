ae969b8aef6768454d3c22bfcdbaaa4ad9c8a56e
mpanoathos <athosmpano427@gmail.com>
2024-02-13 21:31:37 +0200

    Added responsiveness

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -10,17 +10,3 @@ document.addEventListener("DOMContentLoaded", function () {
         hamburgerMenu.classList.toggle("active");
     });
 });
-
-////Ham burger menu
-document.addEventListener("DOMContentLoaded", function () {
-    const navbar = document.querySelector("nav");
-    const hamburgerMenu = document.getElementById("hamburger-menu");
-
-    hamburgerMenu.addEventListener("click", function () {
-        // Toggle the visibility of the navigation bar
-        navbar.classList.toggle("show-navbar");
-
-        // Toggle the appearance of the hamburger menu icon
-        hamburgerMenu.classList.toggle("active");
-    });
-});
