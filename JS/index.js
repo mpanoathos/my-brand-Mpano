8f029179c394a94f3ee2c13e73ce0cd928319bd7
mpanoathos <athosmpano427@gmail.com>
2024-02-14 00:26:55 +0200

    Made some modifications

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,4 @@
+
 //Downloading CV
 document.querySelector('.cv').addEventListener('click',()=>{
     let link =document.createElement('a');
@@ -6,17 +7,4 @@ document.querySelector('.cv').addEventListener('click',()=>{
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-});
-//Ham burger menu
-document.addEventListener("DOMContentLoaded", function () {
-    const navbar = document.querySelector("nav");
-    const hamburgerMenu = document.getElementById("hamburger-menu");
-
-    hamburgerMenu.addEventListener("click", function () {
-        // Toggle the visibility of the navigation bar
-        navbar.classList.toggle("show-navbar");
-
-        // Toggle the appearance of the hamburger menu icon
-        hamburgerMenu.classList.toggle("active");
-    });
 });
--- JS/viewblogs.js
+++ JS/viewblogs.js
@@ -1,12 +1,23 @@
-document.addEventListener('DOMContentLoaded',function(){
-const readMore=document.querySelectorAll('.read-more');
-readMore.forEach(function(btn){
-    btn.addEventListener('click',function(){
-        const target=btn.getAttribute('data-target');
-        const paragraph=document.querySelector(`.${target} .blogs-paragraph`);
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.querySelector("nav");
+    const hamburgerMenu = document.getElementById("hamburger-menu");
+    const readMoreButtons = document.querySelectorAll('.read-more');
 
-        paragraph.style.display=(paragraph.style.display==='none' || paragraph.style.display==='')? 'block':'none';
-        btn.innerText=(paragraph.style.display==='none')? 'Read More' : 'Read Less';
-    });  
-})
+    hamburgerMenu.addEventListener("click", function () {
+        // Toggle the visibility of the navigation bar
+        navbar.classList.toggle("show-navbar");
+
+        // Toggle the appearance of the hamburger menu icon
+        hamburgerMenu.classList.toggle("active");
+    });
+
+    readMoreButtons.forEach(function (btn) {
+        btn.addEventListener('click', function () {
+            const target = btn.getAttribute('data-target');
+            const paragraph = document.querySelector(`.${target} .blogs-paragraph`);
+
+            paragraph.style.display = (paragraph.style.display === 'none' || paragraph.style.display === '') ? 'block' : 'none';
+            btn.innerText = (paragraph.style.display === 'none') ? 'Read More' : 'Read Less';
+        });
+    });
 });
