1f993e0e18f3d84c1f0768325e38ebfbb9163858
mpanoathos <athosmpano427@gmail.com>
2024-02-21 09:52:18 +0200

    fixing bugs on protetction

--- JS/auth.js
+++ JS/auth.js
@@ -2,17 +2,15 @@ class Auth {
     constructor() {
         // Check if user is authenticated
         const auth = localStorage.getItem('auth');
-        const currentPage = window.location.pathname;
-
         if (!this.isAuthenticated(auth)) {
             // Redirect to login page if not authenticated
-            if (currentPage !== '/login.html' && currentPage !== '/signup.html') {
-                this.redirectTo('login.html');
-            }
+            window.location.replace('login.html');
         } else {
-            // Redirect to dashboard if already authenticated and trying to access login or signup
+            // Check if the user is trying to access the login or signup page
+            const currentPage = window.location.pathname;
             if (currentPage == '/login.html' || currentPage == '/signup.html') {
-                this.redirectTo('dashboard.html');
+                // Redirect to dashboard if already authenticated
+                window.location.replace('dashboard.html');
             } else {
                 // Display the body if authenticated and not on login/signup pages
                 document.querySelector('body').style.display = 'block';
@@ -29,13 +27,6 @@ class Auth {
     logOut() {
         // Clear authentication information on logout
         localStorage.removeItem('auth');
-        this.redirectTo('login.html');
-    }
-
-    redirectTo(path) {
-        // Navigate relative to the current page
-        const currentPath = window.location.pathname;
-        const newPath = currentPath.substring(0, currentPath.lastIndexOf('/') + 1) + path;
-        window.location.replace(newPath);
+        window.location.replace('login.html'); 
     }
 }
